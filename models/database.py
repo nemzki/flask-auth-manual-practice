f2f45b1e37d24de4311d79180b00151e16ea7bb2
nemzki <maglawaymark017@gmail.com>
2026-01-14 16:31:25 +0800

    updated users table

--- models/database.py
+++ models/database.py
@@ -6,7 +6,6 @@ def get_db():
     # return a connection to the database with row access by name
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
-
     return conn
 
 # Create users if not exists
@@ -16,32 +15,9 @@ def init_db():
         conn.execute("""
         CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(255) UNIQUE NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
         )
         """)
-
-#  INSERTING USER TO DATABASE
-def add_user(email, password_hash):
-    try:
-        with get_db() as conn:
-
-            conn.execute(
-                "INSERT INTO users (email, password) VALUES (?, ?)",
-                (email, password_hash)
-            )
-
-            return True
-    except Exception as e:
-        print("Error adding user: ", e)
-        return False
-
-
-#   Return a user row by email or None if nort found
-def get_user_by_email(email):
-    db = get_db()
-    user = db.execute(
-        "SELECT * FROM users WHERE email = ?",
-        (email,)
-    ).fetchone()
-    return user
+        conn.commit()
