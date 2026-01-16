ee6eaa9454ed11da61f082ded836ac8834d3a702
nemzki <maglawaymark017@gmail.com>
2026-01-16 14:18:45 +0800

    added first_name and last_name attributes in users table

--- models/database.py
+++ models/database.py
@@ -15,6 +15,8 @@ def init_db():
         conn.execute("""
         CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
         username VARCHAR(255) UNIQUE NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
