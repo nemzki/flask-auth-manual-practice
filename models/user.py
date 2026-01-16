0686e233bfc275ca5af8e0c326fd6004c8551a65
nemzki <maglawaymark017@gmail.com>
2026-01-16 14:20:34 +0800

    updated create_user() and added get_user_by_email() function

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 from .database import get_db
 
-def create_user(username, email, password):
+def create_user(first_name, last_name, username, email, password):
     try:
         with get_db() as conn:
             conn.execute(
-                "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-                (username, email, password)
+                "INSERT INTO users (username, email, password) VALUES (?, ?, ?, ?, ?)",
+                (first_name, last_name, username, email, password)
             )
             conn.commit()
             return True
@@ -18,4 +18,11 @@ def get_user_by_username(username):
         return conn.execute(
                 "SELECT * FROM users WHERE username = ?",
                 (username,)
+            ).fetchone()
+
+def get_user_by_email(email):
+    with get_db() as conn:
+        return conn.execute(
+                "SELECT * FROM users WHERE username = ?",
+                (email,)
             ).fetchone()
