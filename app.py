aacc96866713fb50ff7549cead06032a2b754448
nemzki <maglawaymark017@gmail.com>
2026-01-18 15:03:15 +0800

    reistered aut_bp

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from models.database import init_db
+from routes import auth_bp
 
 #initialize app
 app = Flask(__name__)
@@ -9,6 +10,8 @@ app.secret_key = "practice-flask-auth"
 
 #initialize db
 init_db()
+
+app.register_blueprint(auth_bp, url_prefix="/auth")
 
 if __name__ == '__main__':
     app.run(debug=True)
