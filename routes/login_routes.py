e4d4cd226c2481b2a003d0ab28d73f99f814ae39
nemzki <maglawaymark017@gmail.com>
2026-01-19 16:00:59 +0800

    created login route

--- routes/login_routes.py
+++ routes/login_routes.py
@@ -1,6 +1,37 @@
-from flask import render_template
+# FLASK
+from flask import render_template, redirect, url_for, flash, session
+from werkzeug.security import check_password_hash
+
+# DATABASE
+from models.user import get_user_by_username
+
+# BLUEPRINT
 from . import auth_bp
 
-@auth_bp.route('/login')
+# FORM
+from forms.login_form import LoginForm
+
+@auth_bp.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+
+    form = LoginForm() # 1. Create form intance object of Loginform
+
+    # 2. Validate form
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        # 3. Retrieve User
+        user = get_user_by_username(username)
+
+        # 5. Verify user
+        if user and check_password_hash(user.hash_password, password):
+
+            # 5.1 Create user session
+            session["user_id"] = user.id
+            session["username"] = user.username
+
+            return redirect(url_for('main.educator_dashboard'))
+        flash("Invalid credentials.", "danger")
+
+    return render_template('login.html', form=form)
