b3093d5c2342ff214628b18522780d96f08b3b3f
nemzki <maglawaymark017@gmail.com>
2026-01-19 15:59:24 +0800

    added main_bp for dashboard routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,13 @@
 from flask import Blueprint
 
+# AUTHENTICATION BLUEPRINT
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth", template_folder="templates")
 
-# AUTHENTICATION BLUEPRINT ROUTE IMPORTS
+# MAIN/DASHBOARD BLUEPRINT
+main_bp = Blueprint("main", __name__, url_prefix="/main", template_folder="templates")
+
+# AUTHENTICATION ROUTE IMPORTS
 from . import register_routes
 from . import login_routes
+
+# DASHBOARD ROUTE IMPORTS
